f82c346c3f96a7d36876197aee6991f362c535fd
Your Name <you@example.com>
2021-09-22 14:54:18 -0400

    finish

--- app/src/main/java/com/example/carloancalc/MainActivity.java
+++ app/src/main/java/com/example/carloancalc/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.carloancalc;
 
+import static java.lang.Math.pow;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
    private TextView loanOrLease;
-   private EditText editPrice;
+   private TextView editStatement;
+   private EditText editCarPrice;
    private EditText editPayment;
+   private EditText editRate;
    private Button buttonCalculate;
+   private ToggleButton toggleButton1;
 
 
     @Override
@@ -22,23 +29,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initToggleButton1();
-        setForEditing(false);
+
+        editCarPrice = findViewById(R.id.editcarPrice);
+        editPayment = findViewById(R.id.editPayment);
+        editStatement = findViewById(R.id.editStatement);
+        editRate = findViewById(R.id.editRate);
+        toggleButton1 = findViewById(R.id.toggleButton1);
+        toggleButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(toggleButton1.isChecked()){
+                    double payment = Double.parseDouble(editPayment.getText().toString());
+                    double mr = ( Double.parseDouble(editRate.getText().toString())/100 )/12; //get input from user
+                    double price = Double.parseDouble((editCarPrice.getText().toString()))/3;
+                    double L = price - payment;
+                    int n = 36;
+                    double p= mr*L/(1-Math.pow((1+mr), -n));
+                    //double p2 = Math.round(( p * 100.0) /100 );
+                    editStatement.setText("Your Total Monthly Payment: " + p);
+
+                }
+                else{
+                    buttonCalculate.getText();
+
+
+
+                }
+
+            }
+        });
+
+
+
 
 
         buttonCalculate = findViewById(R.id.buttonCalculate);
         buttonCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-// P = mr*L/(1-(1+mr)-n)
-                int numberPayment;
-                int loanSize;
-                double interestRate;
-                double monthlyPayment;
 
 
-                numberPayment =monthlyPayment*loanSize
+                double mr = ( Double.parseDouble(editRate.getText().toString())/100 )/12; //get input from user
+                double price= Double.parseDouble(editCarPrice.getText().toString());
+                double payment = Double.parseDouble(editPayment.getText().toString());
+                double L = price - payment;
+                int n = 36;
+                double p= mr*L/(1-Math.pow((1+mr), -n));
+                //double p2 = Math.round(( p * 100.0) /100 );
+                editStatement.setText("Your Total Monthly Payment: " + p);
+
+
+
             }
         });
+
+
 
     }
 
@@ -47,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
         toggleButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setForEditing(toggleButton1.isChecked());
+
 
             }
         });
@@ -55,15 +100,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-    private void setForEditing(boolean enabled) {
-        EditText editPayment = findViewById(R.id.editPayment);
-        EditText editPrice = findViewById(R.id.editPrice);
-
-        editPayment.setEnabled(enabled);
-        editPrice.setEnabled(enabled);
-
-
-
-    }
 
 }
